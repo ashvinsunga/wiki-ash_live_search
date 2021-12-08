68b886914153e33e8457673cb5ac1d0cae3e30aa
Ashvin <ashvinsunga@yahoo.com.ph>
2021-12-08 23:13:48 +0800

    first commit

--- index.js
+++ index.js
@@ -1,61 +1,50 @@
-$(document).ready(function () {
-  $('.read').on('click', function () {
-    var url = 'https://animechan.vercel.app/api/random';
-    loading();
+// This function will perform JSON request from API
+function ajaxCall() {
+  $.ajax({
+    url:
+      'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' +
+      $('#search').val(),
+    dataType: 'jsonp',
+    type: 'GET',
+    success: function (data) {
+      //   console.log(data);
+      $('#update').empty();
+      var data = JSON.stringify(data);
+      data = JSON.parse(data);
+      //
+      var output = '';
+      data.query.search.forEach(function (data) {
+        var url =
+          '<a href="https://en.wikipedia.org/wiki/' +
+          data.title +
+          '"' +
+          '" target=_blank">';
+        var title = '<h2>' + data.title + '</h2>' + '<br>';
+        var endUrl = '</a>';
+        var snippet = '<p>' + data.snippet + '</p>' + '<br>';
 
-    $.getJSON(url, function (data) {
-      var currentQuote = data;
-      //   Check the length of the quote
-      if (currentQuote.quote.length > 150) {
-        $('.quoteBody').addClass('longQuote');
-      } else if (currentQuote.quote.length > 450) {
-        $('.quoteBody').addClass('longestQuote');
-      } else {
-        $('.quoteBody').removeClass('longQuote');
-        $('.quoteBody').removeClass('longestQuote');
-      }
-
-      $('.quoteBody').html(currentQuote.quote);
-      $('.quoteAnimeCharacter').html(currentQuote.character);
-      $('.quoteAnime').html(currentQuote.anime);
-      $('.read').html('Next Quote');
-      $('.tweetQuote')
-        .attr(
-          'href',
-          'https://twitter.com/intent/tweet?text=' +
-            currentQuote.quote +
-            '  -' +
-            currentQuote.character
-        )
-        .attr('target', '_blank')
-        .removeClass('disabled');
-      complete();
-    });
+        output += url + title + endUrl + snippet + '<hr>';
+      });
+      $('#update').append(output);
+    },
   });
-  //   Show Loading
-  var loader = document.getElementById('loader');
-  var quoteBody = document.getElementById('quoteBody');
-  var quoteAnimeCharacter = document.getElementById('quoteAnimeCharacter');
-  var quoteAnime = document.getElementById('quoteAnime');
-  var buttons = document.getElementsByClassName('btn');
+}
 
-  function loading() {
-    loader.hidden = false;
-    quoteBody.hidden = true;
-    quoteAnimeCharacter.hidden = true;
-    quoteAnime.hidden = true;
-    buttons[0].hidden = true;
-    buttons[1].hidden = true;
-  }
-  //   Hide Loading
-  function complete() {
-    if (!loader.hidden) {
-      loader.hidden = true;
-      quoteBody.hidden = false;
-      quoteAnimeCharacter.hidden = false;
-      quoteAnime.hidden = false;
-      buttons[0].hidden = false;
-      buttons[1].hidden = false;
-    }
-  }
+function randomWiki() {
+  $('#update').empty();
+  $('#search').empty();
+  $('iframe').attr('src', 'https://www.mediawiki.org/wiki/Special:Random');
+}
+$(document).ready(function () {
+  $('#search').focus();
+  //   $('#search').off('keyup');
+  $('#search').on('keyup', function () {
+    ajaxCall();
+    $('iframe').attr('src', '');
+  });
+  // Show random wiki
+  $('.random').on('click', function () {
+    randomWiki();
+    $(this).text('Show another wiki');
+  });
 });
